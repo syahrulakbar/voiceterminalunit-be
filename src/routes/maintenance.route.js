a8b8f79647be712fd9fc2411a06682f8cf6b60d4
jarfajar2314 <mfajaryusuf@upi.edu>
2023-03-10 14:11:04 +0700

    docs: add few routes docs

--- src/routes/maintenance.route.js
+++ src/routes/maintenance.route.js
@@ -9,10 +9,65 @@ module.exports = function (app) {
 		next();
 	});
 
+	/**
+	 * @swagger
+	 * /api/maintenance/connection-check/ip:
+	 *   get:
+	 *     tags:
+	 *       - Maintenance
+	 *     summary: Check device's IP Address
+	 *     description: Check device's IP Address.
+	 *     responses:
+	 *       200:
+	 *          description: Successfully logged in.
+	 *          content:
+	 *            application/json:
+	 *              schema:
+	 *                type: object
+	 *                properties:
+	 *                  ipAddress:
+	 *                    type: string
+	 *                    description: Device's IP Address.
+	 *                    example: 192.168.6.126
+	 */
 	app.get("/api/maintenance/connection-check/ip", controller.getIPAddress);
+
+	/**
+	 * @swagger
+	 * /api/maintenance/connection-check/status:
+	 *   get:
+	 *     tags:
+	 *       - Maintenance
+	 *     summary: Check device's connection status
+	 *     description: Check device's connection by pinging to Google Open DNS (8.8.8.8).
+	 *     responses:
+	 *       200:
+	 *          description: Device connected to configured IP address.
+	 *          content:
+	 *            application/json:
+	 *              schema:
+	 *                type: object
+	 *                properties:
+	 *                  message:
+	 *                    type: string
+	 *                    description: Message result.
+	 *                    example: |
+	 *                      8.8.8.8: Alive
+	 */
 	app.get(
 		"/api/maintenance/connection-check/status",
 		controller.connectionStatus
 	);
+
+	/**
+	 * @swagger
+	 * /api/maintenance/reboot:
+	 *   post:
+	 *     tags:
+	 *       - Maintenance
+	 *     summary: Reboot device
+	 *     description: Reboot device
+	 */
 	app.post("/api/maintenance/reboot", controller.reboot);
 };
+
