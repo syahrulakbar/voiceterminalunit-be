86d2bec8411730eeea66439ff4ce09feb0ee2472
jarfajar2314 <mfajaryusuf@upi.edu>
2023-03-10 13:02:45 +0700

    docs: add docs for device status route

--- src/routes/deviceStatus.route.js
+++ src/routes/deviceStatus.route.js
@@ -9,5 +9,35 @@ module.exports = function (app) {
 		next();
 	});
 
+	/**
+	 * @swagger
+	 * /api/device-status:
+	 *   get:
+	 *     tags:
+	 *       - Device Status
+	 *     summary: Check device status
+	 *     description: Check device status such as CPU & RAM usage and Temperature.
+	 *     responses:
+	 *       200:
+	 *          description: Get device status success.
+	 *          content:
+	 *            application/json:
+	 *              schema:
+	 *                type: object
+	 *                properties:
+	 *                  memory:
+	 *                    type: number
+	 *                    description: Memory (RAM) usage in %.
+	 *                    example: 68.89
+	 *                  cpu:
+	 *                    type: number
+	 *                    description: CPU usage in %.
+	 *                    example: 84.1
+	 *                  temperature:
+	 *                    type: number
+	 *                    description: CPU temperature in C.
+	 *                    example: 59
+	 */
 	app.get("/api/device-status", controller.getDeviceStatus);
 };
+
