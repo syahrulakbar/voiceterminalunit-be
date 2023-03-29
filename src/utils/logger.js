17307690712239053b0a2f7070363cb76e7bf7e5
Muhammad Fajar <mfajaryusuf@upi.edu>
2023-03-29 16:04:04 +0700

    feat: added time interval for logging

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -2,16 +2,27 @@ const { format, createLogger, transports } = require("winston");
 const { combine, timestamp, printf } = format;
 
 const myFormat = printf(({ level, message, timestamp }) => {
-	return `${new Date().toLocaleString()} ${level}: ${message}`;
+  return `${new Date().toLocaleString()} ${level}: ${message}`;
+});
+
+const monitorFormat = printf(({ level, message, timestamp }) => {
+  return `${new Date().toISOString()} | ${level} | ${message}`;
 });
 
 const combinedFormat = combine(timestamp(), myFormat);
+const combinedMonitorFormat = combine(timestamp(), monitorFormat);
 
 exports.logger = createLogger({
-	level: "info",
-	format: combinedFormat,
-	transports: [
-		new transports.File({ filename: "src/logs/error.log", level: "error" }),
-		new transports.File({ filename: "src/logs/combined.log" }),
-	],
+  level: "info",
+  format: combinedFormat,
+  transports: [
+    new transports.File({ filename: "src/logs/error.log", level: "error" }),
+    new transports.File({ filename: "src/logs/combined.log" }),
+  ],
+});
+
+exports.monitoringLogger = createLogger({
+  level: "info",
+  format: monitorFormat,
+  transports: [new transports.File({ filename: "src/logs/deviceStatus.log" })],
 });
--- src/utils/websocket.js
+++ src/utils/websocket.js
@@ -1,23 +1,67 @@
 const http = require("http");
-const { logger } = require("../utils/logger.js");
+const { logger, monitoringLogger } = require("../utils/logger.js");
 const deviceStatusController = require("../controllers/deviceStatus.controller");
 
 exports.start = (app) => {
-	const server = http.createServer(app);
-	const { Server } = require("socket.io");
-	const io = new Server(server);
+  const server = http.createServer(app);
+  const { Server } = require("socket.io");
+  const io = new Server(server);
 
-	io.on("connection", (socket) => {
-		logger.info("a user connected");
-		setInterval(() => {
-			deviceStatusController.socketGetDeviceStatus().then((status) => {
-				if (status) socket.emit("deviceStatus", status);
-			});
-		}, 1000);
-	});
+  io.on("connection", (socket) => {
+    logger.info("a user connected");
 
-	let PORT = process.env.WEBSOCKET_PORT || 3000;
-	server.listen(PORT, () => {
-		logger.info(`Socket is running on port ${PORT}.`);
-	});
+    let timeout = 1000;
+
+    let monitorFunction = () => {
+      deviceStatusController.socketGetDeviceStatus().then((status) => {
+        if (status) {
+          socket.emit("deviceStatus", status);
+        }
+      });
+    };
+
+    let loggingFunction = () => {
+      deviceStatusController.socketGetDeviceStatus().then((status) => {
+        if (status) {
+          monitoringLogger.info(JSON.stringify(status));
+        }
+      });
+    };
+
+    let monitor = setInterval(monitorFunction, 1000);
+    let logging = setInterval(loggingFunction, timeout);
+
+    socket.on("deviceStatus", (data) => {
+      if (data === "stop") {
+        try {
+          io.in(socket.id).disconnectSockets();
+        } catch (error) {
+          console.log(error);
+        }
+      }
+
+      if (!isNaN(parseInt(data))) {
+        timeout = Number(data) * 1000;
+        clearInterval(logging);
+        logging = setInterval(loggingFunction, timeout);
+      }
+    });
+  });
+
+  let PORT = process.env.WEBSOCKET_PORT || 3000;
+  server.listen(PORT, () => {
+    logger.info(`Socket is running on port ${PORT}.`);
+  });
+};
+
+const startLog = (interval) => {
+  setInterval(() => {
+    deviceStatusController.socketGetDeviceStatus().then((status) => {
+      if (status) {
+        console.log(interval);
+        socket.emit("deviceStatus", status);
+        monitoringLogger.info(JSON.stringify(status));
+      }
+    });
+  }, interval);
 };
