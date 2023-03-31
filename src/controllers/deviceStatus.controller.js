51f50f4d57803b00010a703108cb9be23cf9c056
Muhammad Fajar <mfajaryusuf@upi.edu>
2023-03-31 14:55:50 +0700

    feat: added random number generator

--- src/controllers/deviceStatus.controller.js
+++ src/controllers/deviceStatus.controller.js
@@ -70,7 +70,11 @@ const getCPUTemp = () => {
         resolve(data / 1000);
       });
     } else {
-      resolve(60);
+      resolve(randomIntFromInterval(50, 85));
     }
   });
+};
+
+const randomIntFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 };
