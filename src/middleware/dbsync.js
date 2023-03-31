a623886f46bbe3f692cfbfacac1df2a808aa380e
Muhammad Fajar <mfajaryusuf@upi.edu>
2023-03-31 09:55:43 +0700

    fix: fixed unhandled promise error when start up server

--- src/middleware/dbsync.js
+++ src/middleware/dbsync.js
@@ -5,50 +5,49 @@ const IPConfiguration = db.ipconfiguration;
 const { logger } = require("../utils/logger.js");
 
 const init = () => {
-	db.sequelize.sync({ force: true }).then(() => {
-		initial().then(() => {
-			logger.info("Done syncing database.");
-		});
-	});
+  db.sequelize.sync({ force: true }).then(() => {
+    initial().then(() => {
+      logger.info("Done syncing database.");
+    });
+  });
 
-	const bcrypt = require("bcryptjs");
+  const bcrypt = require("bcryptjs");
 
-	const initial = async () => {
-		User.create({
-			email: "superadmin@mail.com",
-			password: bcrypt.hashSync("123456", 8),
-		}).then((user) => {
-			user.setRoles(1);
-		});
-		User.create({
-			email: "admin12@mail.com",
-			password: bcrypt.hashSync("123456", 8),
-		}).then((user) => {
-			user.setRoles(2);
-		});
-		User.create({
-			email: "admin23@mail.com",
-			password: bcrypt.hashSync("123456", 8),
-		}).then((user) => {
-			user.setRoles(2);
-		});
+  const initial = async () => {
+    Role.create({
+      id: 1,
+      name: "superadmin",
+    });
 
-		Role.create({
-			id: 1,
-			name: "superadmin",
-		});
+    Role.create({
+      id: 2,
+      name: "admin",
+    });
 
-		Role.create({
-			id: 2,
-			name: "admin",
-		});
+    User.create({
+      email: "superadmin@mail.com",
+      password: bcrypt.hashSync("123456", 8),
+    }).then((user) => {
+      user.setRoles(1);
+    });
+    User.create({
+      email: "admin12@mail.com",
+      password: bcrypt.hashSync("123456", 8),
+    }).then((user) => {
+      user.setRoles(2);
+    });
+    User.create({
+      email: "admin23@mail.com",
+      password: bcrypt.hashSync("123456", 8),
+    }).then((user) => {
+      user.setRoles(2);
+    });
 
-		IPConfiguration.create({
-			deviceName: "VTU-1",
-			connectionType: "dhcp",
-		});
-	};
+    IPConfiguration.create({
+      deviceName: "VTU-1",
+      connectionType: "dhcp",
+    });
+  };
 };
 
 module.exports = init;
-
