7c695a0ed3fd683c5de498abca54833f1f9c2033
Mochamad Syahrul Akbar <07tav2akbar@gmail.com>
2023-03-31 13:23:23 +0700

    testing deviceStatus with node-os-utils

--- src/controllers/deviceStatus.controller.js
+++ src/controllers/deviceStatus.controller.js
@@ -1,67 +1,60 @@
-const si = require("systeminformation");
+// const si = require("systeminformation");
+var osu = require("node-os-utils");
 const { logger } = require("../utils/logger.js");
 
 exports.getDeviceStatus = (req, res) => {
-	let status = {};
-
-	si.mem()
-		.then((info) => {
-			status.memory = parseFloat(
-				(info.active / 1000**2).toFixed(2)
-			);
-			si.currentLoad()
-				.then((info) => {
-					status.cpu = parseFloat(info.currentLoad.toFixed(2));
-					si.cpuTemperature()
-						.then((info) => {
-							status.temperature = info.main;
-							res.status(200).send({ ...status });
-						})
-						.catch((err) => {
-							logger.error(err.message);
-							res.status(500).send({
-								message:
-									"Failed to get Device Status. Please check application log.",
-							});
-						});
-				})
-				.catch((err) => {
-					logger.error(err.message);
-				});
-		})
-		.catch((err) => {
-			logger.error(err.message);
-		});
+  let status = {};
+  let allClients = [];
+  let runtime = 1;
+  io.on("connection", (client) => {
+    allClients.push(client);
+    client.on("disconnect", function () {
+      console.log("Got disconnect!");
+      let i = allClients.indexOf(client);
+      allClients.splice(i, 1);
+    });
+    client.on("message", (message) => {
+      runtime = message;
+    });
+    osu
+      .mem()
+      .then((memoryMb) => {
+        status.memory = memoryMb;
+        osu.cpu
+          .usage()
+          .then((cpuPercentage) => {
+            status.cpu = cpuPercentage;
+            res.status(200).send({ ...status });
+          })
+          .catch((err) => {
+            logger.error(err.message);
+          });
+      })
+      .catch((err) => {
+        logger.error(err.message);
+      });
+  });
 };
 
 exports.socketGetDeviceStatus = () => {
-	return new Promise((resolve, reject) => {
-		let status = {};
-
-		si.mem()
-			.then((info) => {
-				status.memory = parseFloat(
-					(info.active / 1000**2).toFixed(2)
-				);
-				si.currentLoad()
-					.then((info) => {
-						status.cpu = parseFloat(info.currentLoad.toFixed(2));
-						si.cpuTemperature()
-							.then((info) => {
-								status.temperature = info.main;
-								resolve(status);
-							})
-							.catch((err) => {
-								logger.error(err.message);
-							});
-					})
-					.catch((err) => {
-						logger.error(err.message);
-					});
-			})
-			.catch((err) => {
-				logger.error(err.message);
-			});
-	});
+  return new Promise((resolve, reject) => {
+    let status = {};
+    osu.mem
+      .info()
+      .then((memoryMb) => {
+        status.memory = memoryMb;
+        osu.cpu
+          .usage()
+          .then((cpuPercentage) => {
+            status.cpu = cpuPercentage;
+            resolve(status);
+          })
+          .catch((err) => {
+            logger.error(err.message);
+          });
+      })
+      .catch((err) => {
+        logger.error(err.message);
+      });
+  });
 };
-
